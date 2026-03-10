07f488b042818981e714123724ac23c1e41d92d4
Djonattan Schneider Tasarz <dstasarz1@minha.fag.edu.br>
2026-03-10 16:10:15 -0300

    Adiciona classe Alemon

---
+++ src/Alemon.java
@@ -0,0 +1,9 @@
+public class Alemon {
+    public static void main(String[] args) {
+        halloWelt();
+    }
+    
+    public static void halloWelt() {
+        System.out.println("Hallo Welt!");
+    }
+}
